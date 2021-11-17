46c1c3eb58719413fe9b6b060e75ff1fb2c39e6f
desmondmiles11111 <68543904+abhishekdraut@users.noreply.github.com>
2021-11-17 21:16:34 +0530

    url  with parameteres

--- src/components/bigCardContainer/bigCardContainer.js
+++ src/components/bigCardContainer/bigCardContainer.js
@@ -1,6 +1,5 @@
-
-function BigCardContainer() {
-  
+function BigCardContainer({book_description,book_short_description,book_title,cover_url,authors,tags} ) {
+  console.log(book_description)
   
   
 
@@ -10,24 +9,24 @@ function BigCardContainer() {
         <div className="counter_container category_name">Book Details</div>
       </div>
 
-      <div className="big_card_container" >
+      <div className="big_card_container">
         <div className="card big">
           <div className="card_inner left_side big">
             <img
               className="card_image big"
-              src="https://imgv2-1-f.scribdassets.com/img/audiobook_square_badge/510417754/396x396/e1986f8af6/1632395555?v=1"
+              src={cover_url}
               alt="big-card"
             />
           </div>
           <div className="card_details right_side big">
             <div className="card_inner right_side big header big">
               <a href="#" className="card_details right_side header big">
-                The Afganisthan paper : A secret history of war.
+                {book_title}
               </a>
             </div>
 
             <div className="card_details right_side aurther big">
-              Written By :<a href="#">Aurther Name</a>
+              Written By :{}
             </div>
             <div className="card_details right_side border big"></div>
             <div className="card_details right_side details big">
@@ -35,70 +34,26 @@ function BigCardContainer() {
                 “THE BOOK CHINA DOESN'T WANT YOU TO READ.”—CNN​
               </div>
               <div className="right_side details summery">
-                A riveting insider's story of how the Party and big money work
-                in China today, by a man who, with his wife, Whitney Duan, rose
-                to the zenith of power and wealth—and then fell out of favor.
-                She was disappeared four years ago. News of this book led to a
-                phone call from Whitney, proof that she's alive.
+                {book_short_description}
               </div>
               <div className="right_side details description">
-                As Desmond Shum was growing up impoverished in China, he vowed
-                his life would be different.
-                <br />
-                <br />
-                Through hard work and sheer tenacity he earned an American
-                college degree and returned to his native country to establish
-                himself in business. There, he met his future wife, the highly
-                intelligent and equally ambitious Whitney Duan who was
-                determined to make her mark within China’s male-dominated
-                society. Whitney and Desmond formed an effective team and, aided
-                by relationships they formed with top members of China’s
-                Communist Party, the so-called red aristocracy, he vaulted into
-                China’s billionaire className. Soon they were developing the
-                massive air cargo facility at Beijing International Airport, and
-                they followed that feat with the creation of one of Beijing’s
-                premier hotels. They were dazzlingly successful, traveling in
-                private jets, funding multi-million-dollar buildings and
-                endowments, and purchasing expensive homes, vehicles, and art.
-                <br />
-                <br />
-                But in 2017, their fates diverged irrevocably when Desmond,
-                while residing overseas with his son, learned that his now
-                ex-wife Whitney had vanished along with three coworkers.
-                <br />
-                <br />
-                This is both Desmond’s story and Whitney’s, because she has not
-                been able to tell it herself. <br />
-                <br />
+                {book_description}
               </div>
             </div>
             <div className="right_side pills">
-              <a className="pill" href=" ">
-                Business
-              </a>
-              <a className="pill" href=" ">
-                Personal memories
-              </a>
-              <a className="pill" href=" ">
-                Asia
-              </a>
-              <a className="pill" href=" ">
-                Business
-              </a>
-              <a className="pill" href=" ">
-                Personal
-              </a>
-              <a className="pill" href=" ">
-                Business
-              </a>
-              <a className="pill" href=" ">
-                Asia
-              </a>
+              
+              {/* {tags.map(function (pill) {
+                return (
+                  <a className="pill" href=" ">
+                    {pill.tag_name}
+                  </a>
+                );
+              })} */}
             </div>
             <div className="right_side metadata">
               <div className="metadata-left">
                 <div className="publisher">
-                  Publisher: <a href="">Business</a>
+                  Publisher: <a href="">{}</a>
                 </div>
                 <div className="isbn">
                   Isbn: <a href="">Asia</a>
@@ -106,7 +61,7 @@ function BigCardContainer() {
               </div>
               <div className className="metadata-right">
                 <div className="released">
-                  Release:<a href="#">Personal memories</a>
+                  Release:<a href="#">{}</a>
                 </div>
                 <div className="formate">
                   Formate: <a href="#">Asia</a>
--- src/pages/bookDemo.js
+++ src/pages/bookDemo.js
@@ -1,16 +1,22 @@
 import BigCardContainer from "../components/bigCardContainer/bigCardContainer";
+import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 function BookDemo(props) {
-    
-    const id=props.match.params.id
-    
-    
-    
-    
-    
-    return(
-        <BigCardContainer />
-    )
-    
+  const params = useParams();
+  const id = params.id;
+
+  const [fullBook, setFullBook] = useState("");
+
+  useEffect(() => {
+    fetch(`https://iifsd.herokuapp.com/books/${id}`)
+      .then((responce) => {
+        return responce.json();
+      })
+      .then((data) => {setFullBook(data)});
+  }, []);
+  
+
+  return <BigCardContainer {...fullBook} />;
 }
 export default BookDemo;
